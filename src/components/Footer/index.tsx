26cf5195400488c0d7da71cdc8aa9ca3803bba98
Jaime Garcia, Jr <40704782+JJgar2725@users.noreply.github.com>
2022-08-01 21:14:46 -0500

    created footer component + stories

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,42 @@
+import { Grid, IconButton, Typography } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import React from "react";
+
+interface FooterProps {}
+
+export function Footer({}: FooterProps) {
+  return (
+    <div>
+      <Grid container justifyContent="space-between" alignItems="center">
+        <Grid item>
+          <Grid container>
+            <Grid item>
+              <IconButton sx={{ color: "#FFFFFF" }}>
+                <LinkedInIcon />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <IconButton sx={{ color: "#FFFFFF" }}>
+                <GitHubIcon />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <IconButton sx={{ color: "#FFFFFF" }}>
+                <InstagramIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Typography
+            sx={{ fontFamily: "Montserrat", fontSize: 14, color: "#FFFFFF" }}
+          >
+            © Jaime Garcia, Jr.
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
