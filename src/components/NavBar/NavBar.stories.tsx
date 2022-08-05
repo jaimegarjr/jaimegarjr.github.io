68bea2844ad77de41b4e8dc71bb4d4afd3da4ab8
Jaime Garcia, Jr <40704782+JJgar2725@users.noreply.github.com>
2022-08-05 08:35:25 -0500

    fixed navbar stories to support routing

--- src/components/NavBar/NavBar.stories.tsx
+++ src/components/NavBar/NavBar.stories.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { NavBar } from "./index";
+import { BrowserRouter } from "react-router-dom";
 
 export default {
   title: "Components/NavBar",
   component: NavBar,
 } as ComponentMeta<typeof NavBar>;
 
-const Template: ComponentStory<typeof NavBar> = (args) => <NavBar />;
+const Template: ComponentStory<typeof NavBar> = (args) => (
+  <BrowserRouter>
+    <NavBar />{" "}
+  </BrowserRouter>
+);
 
 export const InitialState = Template.bind({});
 InitialState.args = {};
